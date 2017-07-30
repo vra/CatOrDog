2cfb675ab8db2acc468b6ce4dbe355d020ee70c9
vra <wangyf11@mail.ustc.edu.cn>
2017-07-30 12:34:13 +0800

    add uikit front end

--- CatOrDog/views.py
+++ CatOrDog/views.py
@@ -1,4 +1,5 @@
 import StringIO
+import random
 
 from PIL import Image
 from keras.models import load_model
@@ -14,9 +15,9 @@ def index(request):
 def demo(request):
 	if request.method == 'POST' and request.FILES['img']:
 		img = request.FILES['img']
-		cls = classifer(img)
+		cls, tmp_im_name = classifer(img)
 		print 'classify result: ', 'dog' if cls else 'cat'
-		render_dict = {'done': 1, 'cls': cls}
+		render_dict = {'done': 1, 'cls': cls, 'tmp_im_name': tmp_im_name}
 		return render(request, 'demo.html', render_dict)
 	else:
 		return render(request, 'demo.html')
@@ -31,6 +32,8 @@ def classifer(img):
 	
 	# chunks to PIL Image, see here: https://stackoverflow.com/questions/24996518/what-size-to-specify-to-pil-image-frombytes
 	img = Image.open(StringIO.StringIO(img_str))
+	tmp_name = str(random.random()).split('.')[1]+'.jpg'
+	img.save('static/medias/'+tmp_name)
 	img = img.resize((150,150))
 	img_np = np.expand_dims(np.asarray(img, dtype='float32'), axis=0)
 	
@@ -39,7 +42,9 @@ def classifer(img):
 	with graph.as_default():
 		model = load_model(model_path)
 		res = model.predict(img_np, batch_size=1)
+	print 'prob:', res[0,0]
 	if res[0,0] < 0.5:
-		return 0
+		cls = 0
 	else:
-		return 1
+		cls = 1
+	return cls, tmp_name
